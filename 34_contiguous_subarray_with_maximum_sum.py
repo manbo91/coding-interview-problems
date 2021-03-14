5ae6d0c0e89685a43a077428426e37936805895a
Seungyeon Kim <manbo91@naver.com>
2021-03-14 11:04:42 +0900

    added 34

---
+++ 34_contiguous_subarray_with_maximum_sum.py
@@ -0,0 +1,41 @@
+"""
+Hi, here's your problem today. This problem was recently asked by Twitter:
+
+You are given an array of integers. Find the maximum sum of all possible
+contiguous subarrays of the array.
+
+Example:
+
+[34, -50, 42, 14, -5, 86]
+
+Given this input array, the output should be 137. The contiguous subarray with
+the largest sum is [42, 14, -5, 86].
+
+Your solution should run in linear time.
+"""
+
+#def max_subarray_sum(arr):
+#    pass
+
+
+def max_subarray_sum(arr):
+    max_sum = float('-inf')
+    current_sum = max_sum
+
+    for num in arr:
+        print(f"num: {num}, current_sum: {current_sum}, max: {max_sum}")
+        if num > current_sum:
+            current_sum = max(num, current_sum + num)
+        else:
+            current_sum += num
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
+
+
+print(max_subarray_sum([34, -50, 42, 14, -5, 86]))
+# 137
+print(max_subarray_sum([1, 1, 1, 2, 3]))
+# 8
+print(max_subarray_sum([-1, 1, -1, 2, -3]))
+# 2
