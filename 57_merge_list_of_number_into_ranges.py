3e169778ab02862a3ff53404c37de91d8c9fccd7
Seungyeon Kim <manbo91@naver.com>
2021-04-06 09:30:47 +0900

    feat: Added 57 problem - Merge List Of Number Into Ranges
    I took 10 minutes to get the solution.
    
    Complexity Analysis:
    - time complexity: O(n)
    - space complexity: O(n)

---
+++ 57_merge_list_of_number_into_ranges.py
@@ -0,0 +1,38 @@
+"""
+Hi, here's your problem today. This problem was recently asked by Facebook:
+
+Given a sorted list of numbers, return a list of strings that represent all
+of the consecutive numbers.
+
+Example:
+Input: [0, 1, 2, 5, 7, 8, 9, 9, 10, 11, 15]
+Output: ['0->2', '5->5', '7->11', '15->15']
+Assume that all numbers will be greater than or equal to 0, and each element
+can repeat.
+
+Here is a starting point:
+
+def findRanges(nums):
+  # Fill this in.
+"""
+
+
+def findRanges(nums):
+    ranges = list()
+    start = nums[0]
+    for i in range(len(nums) - 1):
+        num1 = nums[i]
+        num2 = nums[i + 1]
+        if num2 - num1 <= 1:
+            continue
+        else:
+            ranges.append([f"{start}->{num1}"])
+            start = num2
+    ranges.append([f"{start}->{start}"])
+    return ranges
+
+
+print(findRanges([0, 1, 2, 5, 7, 8, 9, 9, 10, 11, 15]))
+# ['0->2', '5->5', '7->11', '15->15']
+print(findRanges([0]))
+# ['0->0']
